92abe663adcde2d160f50790abdc034042fbf22e
NoodleSushi <ytmikesstuffs@gmail.com>
2023-11-11 00:33:28 +0800

    add dynamic table formatting to ProcessTimeline class

--- process_timeline.py
+++ process_timeline.py
@@ -1,3 +1,4 @@
+import shutil
 from typing import List, Optional
 from dataclasses import dataclass
 from collections.abc import Sequence
@@ -36,14 +37,20 @@ class ProcessTimeline(Sequence):
     
     def __str__(self) -> str:
         out = ""
+        width = shutil.get_terminal_size().columns
         max_name_length = min(4, max(len(task.process.name if task.process else '──') for task in self.__tasks) + 2)
-        border_top = "╭" + "─" * max_name_length + ("┬" + "─" * max_name_length) * (len(self.__tasks) - 1) + "╮"
-        border_bot = "╰" + "─" * max_name_length + ("┴" + "─" * max_name_length) * (len(self.__tasks) - 1) + "╯"
-        out += border_top + "\n"
-        for task in self.__tasks:
-            out += f"│{(task.process.name if task.process else '──').center(max_name_length)}"
-        out += f"│\n{border_bot}\n"
-        for task in self.__tasks:
-            out += str(task.start).ljust(max_name_length + 1)
-        out += f"{str(task.end).ljust(max_name_length)}\n"
+        max_cell_length = max_name_length + 1
+        cells_per_row = (width // max_cell_length) - 1
+        begin_spaces = " " * ((width - ((max_cell_length * cells_per_row) + len(str(self.__tasks[-1].end)))) // 2)
+        for i in range(0, len(self.__tasks), cells_per_row):
+            tasks = self.__tasks[i:i + cells_per_row]
+            border_top = begin_spaces + "╭" + "─" * max_name_length + ("┬" + "─" * max_name_length) * (len(tasks) - 1) + "╮"
+            border_bot = begin_spaces + "╰" + "─" * max_name_length + ("┴" + "─" * max_name_length) * (len(tasks) - 1) + "╯"
+            out += border_top + "\n" + begin_spaces
+            for task in tasks:
+                out += f"│{(task.process.name if task.process else '──').center(max_name_length)}"
+            out += f"│\n{border_bot}\n{begin_spaces}"
+            for task in tasks:
+                out += str(task.start).ljust(max_name_length + 1)
+            out += f"{str(task.end).ljust(max_name_length)}\n"
         return out
